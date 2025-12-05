56453c9e83399818da414af81c8efb8c84f84f5a
Trương Vĩnh Phát <160555191+vinhphat2005@users.noreply.github.com>
2025-12-05 16:59:50 +0700

    chore: Temporarily commit clean dist/ to fix Cloudflare deployment

---
+++ dist/env-config.js
@@ -0,0 +1,11 @@
+// This file will be replaced by inject-env.js script during build
+window._env_ = {
+  EXPO_PUBLIC_API_URL: "https://app-cook-backend.onrender.com",
+  EXPO_PUBLIC_FIREBASE_API_KEY: "",
+  EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: "",
+  EXPO_PUBLIC_FIREBASE_PROJECT_ID: "",
+  EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: "",
+  EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "",
+  EXPO_PUBLIC_FIREBASE_APP_ID: "",
+  EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME: ""
+};
